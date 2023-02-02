0f9b6abcc5cb38d6d3e5ddd70a28382eac0a12c6
CynicalMan <omar.eladly2020@gmail.com>
2023-02-02 02:06:05 +0200

    little-touches

--- src/components/Trending.vue
+++ src/components/Trending.vue
@@ -2,7 +2,7 @@
     <div class="md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative">
         <input type="text" class="pl-12 rounded-full w-full p-2 bg-lighter text-sm" placeholder="Search Twitter">
         <i class="fas fa-search absolute left-0 top-0 mt-5 ml-12 text-sm text-light"></i>
-        <div class="w-full rounded-lg bg-lightest p-3">
+        <div class="w-full rounded-lg bg-lightest p-3 mt-4">
             <div class="flex items-center justify-between p-3">
                 <p class="text-lg font-bold">Trends for You</p>
                 <i class="fas fa-cog text-lg text-blue"></i>
